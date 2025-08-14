e25bac45bdaea77ab1b458aa3bb78a7a41f5d818
rajesh00ai <rajesh007ai@gmail.com>
2025-08-14 10:45:25 +0530

    Fix Our Story Image Import Error

--- client/src/components/our-story.tsx
+++ client/src/components/our-story.tsx
@@ -1,4 +1,4 @@
-import traditionalImagePath from "@assets/img 6_1754285845405.png";
+import traditionalImagePath from "@assets/SARIRA-03_1754285266680.png";
 
 export default function OurStory() {
   return (
